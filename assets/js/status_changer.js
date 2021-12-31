4002b276b4620e27688989432c9eb00c458c536a
John McCall <john@lowlydba.com>
2021-12-31 16:52:10 -0500

    text decorate names from button status
    - Line-through when x'd
    - bolded when checked

--- assets/js/status_changer.js
+++ assets/js/status_changer.js
@@ -8,16 +8,26 @@ function statusButtonChanger (control) {
 
   let index = Data.map(function (e) { return e.value }).indexOf(control.value)
   index++
-  if (index >= Data.length) {
+  if ((index) >= Data.length) {
     index = 0
   }
   control.value = Data[index].value
+  const row = $(control).closest('tr') // eslint-disable-line no-undef
+  switch (Data[index].status) {
+    case 'x':
+      row.toggleClass('x').siblings().removeClass()
+      break
+    case 'checked':
+      row.toggleClass('checked').siblings().removeClass()
+      break
+    default:
+      row.removeClass()
+  }
 }
 
 // Attach function to all checkboxes
 window.onload = function () {
   const elements = document.getElementsByClassName('multi-checkbox')
-
   for (let i = 0; i < elements.length; i++) {
     elements[i].onclick = function () {
       statusButtonChanger(this)
